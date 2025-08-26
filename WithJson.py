647bfb477f4e4889108e2d4997a3441cad5053cf
testuserr077-droid <testuserr077@gmail.com>
2025-08-27 00:36:01 +0530

    Update WithJson.py

--- WithJson.py
+++ WithJson.py
@@ -4,7 +4,7 @@ import os, json, re
 from rapidfuzz import process
 
 app = Flask(__name__)
-key = os.environ.get("OPENAI_API_KEY")
+key = os.environ("OPENAI_API_KEY")
 print("OpenAI Key Loaded:", len(key) if key else "MISSING")
 # Initialize OpenAI client
 client = OpenAI(api_key=os.environ.get["OPENAI_API_KEY"])
